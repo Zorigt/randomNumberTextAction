3e1561ad53d2f897e2da672e15616b1b1ba3114f
Zorigt <Zorigt@gmail.com>
2014-06-22 20:25:30 -0700

    Initial Commit

---
+++ randomNumberText/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  randomNumberText
+//
+//  Created by Baz on 6/22/14.
+//  Copyright (c) 2014 Deal Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
