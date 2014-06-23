27ff50cfcdf32fbbd032993444389aad9724d075
Zorigt <Zorigt@gmail.com>
2014-06-22 21:05:57 -0700

    random number text action

--- randomNumberText/ViewController.h
+++ randomNumberText/ViewController.h
@@ -8,6 +8,17 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    
+    IBOutlet UILabel *label;
+    IBOutlet UILabel *textLabel;
+    IBOutlet UIButton *one;
+    IBOutlet UIButton *two;
+    IBOutlet UIButton *three;
+    IBOutlet UIButton *four;
+}
+- (IBAction)randomNumber:(id)sender;
+- (IBAction)randomText:(id)sender;
+- (IBAction)randomAction:(id)sender;
 
 @end
--- randomNumberText/ViewController.m
+++ randomNumberText/ViewController.m
@@ -14,10 +14,14 @@
 
 @implementation ViewController
 
+int randomNumber;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self randomText:self];
+    [self randomNumber:self];
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +30,66 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)randomNumber:(id)sender {
+    randomNumber = arc4random()%10 + 1;
+    
+    label.text = [[NSString alloc] initWithFormat:@"%d", randomNumber];
+}
+
+- (IBAction)randomText:(id)sender {
+    NSString *textString;
+    NSArray *textArray;
+    
+    textArray = [NSArray arrayWithObjects:
+                 @"One",
+                 @"Two",
+                 @"Three",
+                 @"Four",
+                 @"Five",
+                 @"Six",
+                 @"Seven",
+                 @"Eight",
+                 @"Nine",
+                 @"Ten",
+                 nil];
+    
+    randomNumber = arc4random()%[textArray count];
+    
+    textString = [textArray objectAtIndex:randomNumber];
+    
+    textLabel.text = textString;
+}
+
+- (IBAction)randomAction:(id)sender {
+    randomNumber = arc4random()%6;
+    
+    switch (randomNumber) {
+        case 0:
+            one.hidden = YES;
+            break;
+            
+        case 1:
+            two.hidden = YES;
+            break;
+            
+        case 2:
+            three.hidden = YES;
+            break;
+            
+        case 3:
+            four.hidden = YES;
+            break;
+            
+        case 4:
+            [self randomNumber:self];
+            break;
+            
+        case 5:
+            [self randomText:self];
+            break;
+            
+        default:
+            break;
+    }
+}
 @end
